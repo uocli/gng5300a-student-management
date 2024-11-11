b29f43dbd9372759352ab4a969d262d8dffd20fa
Sean Li <cli049@uottawa.ca>
2024-11-11 15:02:36 -0500

    Removed app called app

--- student_management/app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Student
-
-
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ("first_name", "last_name", "enrollment_date")
-
-
-admin.site.register(Student, StudentAdmin)
