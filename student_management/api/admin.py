ed7d709e0384b5d38bff208220651426b5ff89ce
Sean Li <cli049@uottawa.ca>
2024-11-11 15:11:04 -0500

    Registered Student to admin panel

--- student_management/api/admin.py
+++ student_management/api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Student
 
-# Register your models here.
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = (
+        "first_name",
+        "last_name",
+        "enrollment_date",
+    )
+
+
+admin.site.register(Student, StudentAdmin)
