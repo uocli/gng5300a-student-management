1f6c0cf1f213938f1afca60cc40422749bf3d754
Sean Li <cli049@uottawa.ca>
2024-11-11 15:12:22 -0500

    Added student list view

--- student_management/api/views.py
+++ student_management/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Student
 
-# Create your views here.
+
+# Display a list of all students
+class StudentListView(ListView):
+    model = Student
+    template_name = "student_list.html"
+    context_object_name = "students"
+
