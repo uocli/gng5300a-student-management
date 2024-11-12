a5a958afdd9fd58026e94764f3296dd5193c3716
Sean Li <cli049@uottawa.ca>
2024-11-11 21:56:18 -0500

    Revert "Updated for hosting"
    This reverts commit f26741ea011f8bd5c956f9b4a0b8939a58c9aacb.

--- student_management/student_management/settings.py
+++ student_management/student_management/settings.py
@@ -1,6 +1,4 @@
-import os
 from pathlib import Path
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -15,10 +13,7 @@ SECRET_KEY = "django-insecure-12r!pt-%r##2%g!kn9hdf429j@fp(y552s+)cqwqf-v+8r)+u0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "http://127.0.0.1:8000/",
-    "https://student-management-tkhh.onrender.com",
-]
+ALLOWED_HOSTS = []
 
 # Redirect to the login page if user is not authenticated
 LOGIN_URL = "login"
@@ -72,17 +67,13 @@ WSGI_APPLICATION = "student_management.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-ENVIRONMENT = os.getenv("ENVIRONMENT", "development")
-
-if ENVIRONMENT == "production":
-    DATABASES = {"default": dj_database_url.config(conn_max_age=600)}
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
